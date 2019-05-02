7c5ee642b01a0a3fadd6615703a923a8096174f7
Giles Smith <giles@superrb.com>
2019-05-02 15:08:36 +0100

    Initial Commit

---
+++ MemcachedHandlerBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Superrb\MemcachedHandlerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MemcachedHandlerBundle
+ * @package Superrb\MemcachedHandlerBundle
+ */
+class MemcachedHandlerBundle extends Bundle
+{
+}
